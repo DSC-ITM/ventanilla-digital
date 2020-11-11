e5d3aa6027ea9a3d4b42cc978c3ff2ca1721115c
Raul Novelo <rnovelo.cruz98@gmail.com>
2020-11-11 13:53:49 -0600

    Updates

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+importScripts("/digital-window/precache-manifest.49ef96b0b46cc6b069739e858bc1040d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.setConfig({
+  debug: false
+})
+
+workbox.precaching.precacheAndRoute([])
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+      })
+    ]
+  })
+)
+
+workbox.routing.registerRoute(
+  new RegExp('https://digital-window.herokuapp.com'),
+  workbox.strategies.networkFirst({
+    cacheName: 'api'
+  })
+)
+
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'googleapis',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30
+      })
+    ]
+  })
+)
+
